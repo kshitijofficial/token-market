59f89afe73f3149d7672b6ffb672866d84f357c0
Kshitij Srivastava <ksh@Kshitijs-MacBook-Air.local>
2026-06-14 14:22:18 +0530

    test: fuzz test the token to buy from marketplace

--- test/TokenMarkeplace.t.sol
+++ test/TokenMarkeplace.t.sol
@@ -5,53 +5,96 @@ import {Test} from "forge-std/Test.sol";
 import {TokenMarketplace} from "../src/TokenMarketplace.sol";
 
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
-import "forge-std/console.sol";   
+import "forge-std/console.sol";
 
 contract TokenMarketplaceTest is Test {
     TokenMarketplace public tokenMarketplace;
     ERC20Mock public erc20Mock;
 
-     error TokenMarketplace_ZeroNumberOfTokens(uint256 numberOfTokens);
-
+    error TokenMarketplace_ZeroNumberOfTokens(uint256 numberOfTokens);
 
     function setUp() public {
         address owner = makeAddr("owner");
         erc20Mock = new ERC20Mock();
-        tokenMarketplace = new TokenMarketplace(address(erc20Mock),owner);
-        erc20Mock.mint(address(tokenMarketplace),1000);
+        tokenMarketplace = new TokenMarketplace(address(erc20Mock), owner);
+        erc20Mock.mint(address(tokenMarketplace), 1000);
     }
 
     function testBuyTokensFromMarketplace() public {
         uint256 tokensToBuyFromMarketplace = 2;
         uint256 tokenPrice = tokenMarketplace.TOKEN_PRICE();
-        uint256 totalPriceToPayToBuyTokens = tokensToBuyFromMarketplace*tokenPrice;
-        uint256 tokenMarketplaceEthBalanceBefore = address(tokenMarketplace).balance;
+        uint256 totalPriceToPayToBuyTokens = tokensToBuyFromMarketplace *
+            tokenPrice;
+        uint256 tokenMarketplaceEthBalanceBefore = address(tokenMarketplace)
+            .balance;
         address buyer = makeAddr("buyer");
         uint256 tokenBalanceOfBuyerBefore = erc20Mock.balanceOf(buyer);
-       
+
         vm.prank(buyer);
-        vm.deal(buyer,10 ether);
-        tokenMarketplace.buyTokensFromMarketplace{value: totalPriceToPayToBuyTokens}(tokensToBuyFromMarketplace);
-        uint256 tokenMarketplaceEthBalanceAfter = address(tokenMarketplace).balance;
+        vm.deal(buyer, 10 ether);
+        tokenMarketplace.buyTokensFromMarketplace{
+            value: totalPriceToPayToBuyTokens
+        }(tokensToBuyFromMarketplace);
+        uint256 tokenMarketplaceEthBalanceAfter = address(tokenMarketplace)
+            .balance;
         uint256 tokenBalanceOfBuyerAfter = erc20Mock.balanceOf(buyer);
 
-        assertEq(tokenMarketplaceEthBalanceAfter-tokenMarketplaceEthBalanceBefore, totalPriceToPayToBuyTokens);
-        assertEq(tokenBalanceOfBuyerAfter-tokenBalanceOfBuyerBefore, tokensToBuyFromMarketplace);
+        assertEq(
+            tokenMarketplaceEthBalanceAfter - tokenMarketplaceEthBalanceBefore,
+            totalPriceToPayToBuyTokens
+        );
+        assertEq(
+            tokenBalanceOfBuyerAfter - tokenBalanceOfBuyerBefore,
+            tokensToBuyFromMarketplace
+        );
     }
 
     function test_RevertsWhenNumberOfTokensToBuyFromMarkeplaceIsZero() public {
         uint256 tokensToBuyFromMarketplace = 0;
         address buyer = makeAddr("buyer");
-        vm.deal(buyer,10 ether);
+        vm.deal(buyer, 10 ether);
         vm.prank(buyer);
 
-        vm.expectRevert(abi.encodeWithSelector(TokenMarketplace_ZeroNumberOfTokens.selector,tokensToBuyFromMarketplace));
-        tokenMarketplace.buyTokensFromMarketplace{value: 1 ether}(tokensToBuyFromMarketplace);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                TokenMarketplace_ZeroNumberOfTokens.selector,
+                tokensToBuyFromMarketplace
+            )
+        );
+        tokenMarketplace.buyTokensFromMarketplace{value: 1 ether}(
+            tokensToBuyFromMarketplace
+        );
     }
 
+    function test_FuzzBuyTokensFromMarketplace(
+        uint256 tokensToBuyFromMarketplace
+    ) public {
+        // vm.assume(tokensToBuyFromMarketplace < 1000);
+        tokensToBuyFromMarketplace = bound(tokensToBuyFromMarketplace, 1, 1000);
+        uint256 tokenPrice = tokenMarketplace.TOKEN_PRICE();
+        uint256 totalPriceToPayToBuyTokens = tokensToBuyFromMarketplace *
+            tokenPrice;
+        uint256 tokenMarketplaceEthBalanceBefore = address(tokenMarketplace)
+            .balance;
+        address buyer = makeAddr("buyer");
+        uint256 tokenBalanceOfBuyerBefore = erc20Mock.balanceOf(buyer);
 
+        vm.prank(buyer);
+        vm.deal(buyer, totalPriceToPayToBuyTokens);
+        tokenMarketplace.buyTokensFromMarketplace{
+            value: totalPriceToPayToBuyTokens
+        }(tokensToBuyFromMarketplace);
+        uint256 tokenMarketplaceEthBalanceAfter = address(tokenMarketplace)
+            .balance;
+        uint256 tokenBalanceOfBuyerAfter = erc20Mock.balanceOf(buyer);
 
-    
-
-  
+        assertEq(
+            tokenMarketplaceEthBalanceAfter - tokenMarketplaceEthBalanceBefore,
+            totalPriceToPayToBuyTokens
+        );
+        assertEq(
+            tokenBalanceOfBuyerAfter - tokenBalanceOfBuyerBefore,
+            tokensToBuyFromMarketplace
+        );
+    }
 }
