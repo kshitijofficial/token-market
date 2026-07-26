4a43f956a54a14164cc6146f14c8993aaffdb2a1
Kshitij <srivastavakshitijprofessional@gmail.com>
2026-07-26 07:25:40 +0000

    feat: add get token price

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import abi from "../src/contractAbi/tokenMarketplaceAbi.json";
 // import "./App.css";
@@ -6,6 +6,7 @@ import abi from "../src/contractAbi/tokenMarketplaceAbi.json";
 function App() {
   const [address, setAddress] = useState("");
   const [contract, setContract] = useState("");
+  const [tokenPrice, setTokenPrice] = useState("");
 
   async function connectWallet() {
     if (!window.ethereum) {
@@ -17,7 +18,7 @@ function App() {
 
       const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
       const provider = new ethers.BrowserProvider(window.ethereum);
-      const signer = provider.getSigner();
+      const signer = await provider.getSigner();
 
       const contract = new ethers.Contract(contractAddress, abi, signer);
       setContract(contract);
@@ -25,10 +26,21 @@ function App() {
     }
   }
 
+  useEffect(() => {
+    if (!contract) return;
+    async function getTokenPriceInEth() {
+      const tokenPriceInWei = await contract.getTokenPrice();
+      const tokenPriceInEth = ethers.formatEther(tokenPriceInWei);
+      setTokenPrice(tokenPriceInEth);
+    }
+    getTokenPriceInEth();
+  }, [contract]);
+
   return (
     <>
       <button onClick={connectWallet}>Connect Wallet</button>
       <p>Connected Account: {address}</p>
+      <p>Token Price(In Eth): {tokenPrice} </p>
     </>
   );
 }
